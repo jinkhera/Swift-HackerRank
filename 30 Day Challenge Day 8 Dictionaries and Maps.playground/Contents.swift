3a8781d4c84dbb1a3719807a122bae744294837d
Jatinder Pal Singh Khera <jatinderpalsinghkhera@Jatinders-MacBook-Air.local>
2019-09-25 16:01:58 +0100

    Fix runtime error

--- 30 Day Challenge Day 8 Dictionaries and Maps.playground/Contents.swift
+++ 30 Day Challenge Day 8 Dictionaries and Maps.playground/Contents.swift
@@ -1,5 +1,3 @@
-//: Playground - noun: a place where people can play
-
 // Enter your code here. Read input from STDIN. Print output to STDOUT
 import Foundation
 
@@ -8,21 +6,24 @@ guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!
 var dict = [String: String]()
 for _ in 0..<n {
     guard let temp = readLine() else { fatalError("Bad input") }
-    // print(temp)
     let nameNumber = temp.split(separator: " ")
-    dict[String(nameNumber[0])] = String(nameNumber[1])
+    if nameNumber.count == 2 {
+        dict[String(nameNumber[0])] = String(nameNumber[1])
+    }
 }
 
 for _ in 0..<n {
-    guard let name = readLine() else { fatalError("Bad input") }
-    if dict.keys.contains(String(name)) {
-        if let number = dict[name] {
-            print("\(name)=\(number)")
+    if let name = readLine() {
+        if dict.keys.contains(String(name)) {
+            if let number = dict[name] {
+                print("\(name)=\(number)")
+            }
+        } else {
+            print("Not found")
         }
-    } else {
-        print("Not found")
     }
 }
 
 // print(n)
 // print(dict)
+
