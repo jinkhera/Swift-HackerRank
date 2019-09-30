39768d593e71266edd4758b3169b54715892420e
Jatinder Pal Singh Khera <jatinderpalsinghkhera@Jatinders-MacBook-Air.local>
2019-09-30 17:38:09 +0100

    Fixed! Aaaaarghhh

--- Minimum Bribes.playground/Contents.swift
+++ Minimum Bribes.playground/Contents.swift
@@ -29,18 +29,18 @@
 //
 //minimumBribes(q: q)
 
-//let q = [2, 1, 5, 3, 4]
+let q = [2, 1, 5, 3, 4]
 //let q = [2, 5, 1, 3, 4]
 //let q = [1, 2, 5, 3, 4, 7, 8, 6]
-let q = [1, 2, 5, 3, 7, 8, 6, 4]
+//let q = [1, 2, 5, 3, 7, 8, 6, 4]
 
 // 2nd attempt was to number of times a person bribed
 func minimumBribes(q: [Int]) -> Void {
     var bribes = 0
     
-    var i = 1
+    var i = 0
     for person in q {
-        let placesMoved = person - i
+        let placesMoved = person - (i + 1)
         
         if placesMoved > 2 {
             // not possible, moved too far
@@ -48,19 +48,15 @@ func minimumBribes(q: [Int]) -> Void {
             break;
         }
         else {
-            // now check how many times overtaken/bribed
-            if person < i {
-                for j in person..<q.count {
+            let j = max(0, person - 2)
+            //print("\(j) \(i)")
+            for k in j..<i+1 {
+                let l = q[k]
+                if l > person {
                     bribes += 1
-                    //print("person = \(person) j = \(j) q = \(q[j])")
-                    if person == q[j]    {
-//                        print("person = \(person) j = \(j)")
-                        break
-                    }
                 }
-            } else {
-                //print("Person = \(person) Not bribed")
             }
+            
         }
         
         i += 1
